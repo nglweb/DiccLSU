eda87335e6789856a87bc309deb6953b56e003cc
NicoGimenez <nicolas.gimenezlemos@gmail.com>
2019-05-22 17:24:57 -0300

    1

---
+++ js.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    
+    $('#primero').mouseover(function(){
+        $('#btn-primero').fadeIn();
+        
+    })
+    $('#segundo').mouseover(function(){
+        $('#btn-segundo').fadeIn();
+        
+    })
+    $('#segundo').mouseover(function(){
+        $('#btn-tercero').fadeIn();
+        
+    })
+    $('#profund').mouseover(function(){
+        $('#btn-prof').fadeIn();
+        
+    })
+});
